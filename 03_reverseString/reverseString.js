26b8a072d8270d2d1b7ff3a0d8851ce68eb47eee
jtphillips215 <jtphillips215@gmail.com>
2022-08-27 13:42:57 -0500

    passing first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let charArray = str.split("");
+  let revArray = charArray.reverse();
+  return revArray.join("");
 };
 
 // Do not edit below this line
