915a9079f3b98090931be6dba4f07392a2a79a6d
jtphillips215 <jtphillips215@gmail.com>
2022-08-27 14:34:54 -0500

    function removing single element from array, passing initial test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, i) {
+  let index = arr.indexOf(i);
+  arr.splice(index, 1);
+  return arr;
 };
 
 // Do not edit below this line
