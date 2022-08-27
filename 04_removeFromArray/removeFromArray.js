906f349cca85bbc036b60e576a999089ce9b840f
jtphillips215 <jtphillips215@gmail.com>
2022-08-27 16:35:41 -0500

    passing fifth test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,29 @@
-const removeFromArray = function(arr, i, j) {
+const removeFromArray = function(arr, i, j, k, x) {
   
-  if (i != undefined && !isNaN(i)) {
+  if (i != undefined) {
     let index = arr.indexOf(i);
     if (index != -1) {
       arr.splice(index, 1);
     }
   }
-  if (j != undefined && !isNaN(j)) {
+  if (j != undefined) {
     index = arr.indexOf(j);
     if (index != -1) {
       arr.splice(index, 1);
     }
   } 
+  if (k != undefined) {
+    index = arr.indexOf(k);
+    if (index != -1) {
+      arr.splice(index, 1);
+    }
+  }
+  if (x != undefined) {
+    index = arr.indexOf(x);
+    if (index != -1) {
+      arr.splice(index, 1);
+    }
+  }
   return arr;
 };
 
