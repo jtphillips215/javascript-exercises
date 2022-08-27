6740d103d394c9e59977f5e95821ce0792fcc31b
jtphillips215 <jtphillips215@gmail.com>
2022-08-27 14:38:10 -0500

    passing test for one value and multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function(arr, i) {
+const removeFromArray = function(arr, i, j) {
   let index = arr.indexOf(i);
   arr.splice(index, 1);
+  if (j != undefined) {
+    index = arr.indexOf(j);
+    arr.splice(index, 1);
+  } 
   return arr;
 };
 
