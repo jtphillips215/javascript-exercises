6648a3635f63a11fc3c839aa9c35a05a9c849213
jtphillips215 <jtphillips215@gmail.com>
2022-08-28 15:29:31 -0500

    added type of tests to validate if input is a number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
 
+  if (typeof(num1) != 'number' || typeof(num2) != 'number') {
+    console.log('not a number');
+    return 'ERROR';
+  }
+
   if (num1 < 0 || num2 < 0){
     return 'ERROR';
   }
