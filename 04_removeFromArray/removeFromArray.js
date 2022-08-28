37210a903bbe81e9e6a8cc2a15b36779e8cda97e
jtphillips215 <jtphillips215@gmail.com>
2022-08-28 14:25:37 -0500

    updates function using rest operator

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,30 +1,12 @@
-const removeFromArray = function(arr, i, j, k, x) {
-  
-  if (i != undefined) {
-    let index = arr.indexOf(i);
-    if (index != -1) {
-      arr.splice(index, 1);
+const removeFromArray = function(...args) {
+  let array = args[0];
+  let newArray = [];
+  array.forEach((item) => {
+    if(!args.includes(item)) {
+      newArray.push(item);
     }
-  }
-  if (j != undefined) {
-    index = arr.indexOf(j);
-    if (index != -1) {
-      arr.splice(index, 1);
-    }
-  } 
-  if (k != undefined) {
-    index = arr.indexOf(k);
-    if (index != -1) {
-      arr.splice(index, 1);
-    }
-  }
-  if (x != undefined) {
-    index = arr.indexOf(x);
-    if (index != -1) {
-      arr.splice(index, 1);
-    }
-  }
-  return arr;
+  });
+  return newArray;
 };
 
 // Do not edit below this line
