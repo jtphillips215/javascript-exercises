2028e505bd3a29e697ff3f42f4968793dc5f67f7
jtphillips215 <jtphillips215@gmail.com>
2022-08-28 14:51:20 -0500

    testing for negative numbers, passing fourth test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
+
+  if (num1 < 0 || num2 < 0){
+    return 'ERROR';
+  }
+
   if (num1 > num2) {
     for(let i = num2; i <= num1; i++) {
       sum += i;
