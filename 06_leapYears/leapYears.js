700d7ceebb35c081e529b368dc53a1af985056ff
jtphillips215 <jtphillips215@gmail.com>
2022-08-28 17:52:54 -0500

    passing test for century years now

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,15 @@
 const leapYears = function (year) {
   if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
 // Do not edit below this line
