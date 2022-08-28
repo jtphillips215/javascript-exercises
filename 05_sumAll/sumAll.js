552c1d792a71ba222a2e204a2700c2db5113a30b
jtphillips215 <jtphillips215@gmail.com>
2022-08-28 14:49:16 -0500

    testing which number is smaller, passing through third test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,14 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
-  for(let i = num1; i <= num2; i++) {
-    sum += i;
+  if (num1 > num2) {
+    for(let i = num2; i <= num1; i++) {
+      sum += i;
+    }
+  }
+  else {
+    for(let i = num1; i <= num2; i++) {
+      sum += i;
+    }
   }
   return sum;
 };
