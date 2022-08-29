3c63b75a87c827044964a63f20d6179c2df5f705
jtphillips215 <jtphillips215@gmail.com>
2022-08-28 19:45:30 -0500

    added ctof and ftoc conversion formulas and rounding to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  return Math.round((temp - 32) * (5 / 9));
 };
 
-const ctof = function() {
-
+const ctof = function () {
+  return Math.round(temp * (9 / 5) + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
