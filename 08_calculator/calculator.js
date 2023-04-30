f2ad5d0f84866dbefcba5a2a16e12b7f5f54c80f
jtphillips215 <jtphillips215@gmail.com>
2023-04-30 15:59:07 -0500

    passing all test cases for sum array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,7 +6,13 @@ const subtract = function (num1, num2) {
   return num1 - num2;
 };
 
-const sum = function (num1, num2) {};
+const sum = function (numArray) {
+  let total = 0;
+  numArray.forEach((num) => {
+    total += num;
+  });
+  return total;
+};
 
 const multiply = function () {};
 
