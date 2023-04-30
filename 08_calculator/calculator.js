971678f126d8e0ae150fe4a4c8dac9a3c1258c9d
jtphillips215 <jtphillips215@gmail.com>
2023-04-30 15:52:53 -0500

    completed tests for add

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,18 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
-};
+const sum = function () {};
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +21,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
