f82f6d044d8abe948551513ebcf175bb1804aedb
jtphillips215 <jtphillips215@gmail.com>
2023-04-30 16:29:22 -0500

    passing test case 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let splitString = string.toLowerCase().split();
+  reversedString = splitString.reverse();
+  comparisonString = reversedString.join();
+  return string == comparisonString;
 };
 
 // Do not edit below this line
