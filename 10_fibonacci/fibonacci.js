53baaf3f63ae65894c3fca5fbdd44b2a35c535b2
jtphillips215 <jtphillips215@gmail.com>
2023-05-03 15:14:54 -0500

    passing test case 6

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
 const fibonacci = function (n) {
-  if (n <= 1) {
+  if (n < 0) {
+    return "OOPS";
+  } else if (n <= 1) {
     return n;
   } else {
     return fibonacci(n - 1) + fibonacci(n - 2);
