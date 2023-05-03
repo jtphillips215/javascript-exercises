388a0081516130aba2c348c68e437e635254ec2e
jtphillips215 <jtphillips215@gmail.com>
2023-05-03 15:52:09 -0500

    passing test case

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  booktitles = [];
+  for (let i = 0; i < books.length; i++) {
+    booktitles.push(books[i].title);
+  }
+  return booktitles;
 };
 
 // Do not edit below this line
