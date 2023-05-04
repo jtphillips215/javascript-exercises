c775521f1174ab09ffa152c75a1bd126484d021c
jtphillips215 <jtphillips215@gmail.com>
2023-05-03 19:53:30 -0500

    arrow function to bubble sort array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.sort((a, b) => {
+    const previous = a.yearOfDeath - a.yearOfBirth;
+    const current = b.yearOfDeath - b.yearOfBirth;
+    return a > b ? 1 : -1;
+  });
 };
 
 // Do not edit below this line
