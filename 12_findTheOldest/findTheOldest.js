f379c664947fb3433ac80fb35120e9ff7ef6285b
jtphillips215 <jtphillips215@gmail.com>
2023-05-03 20:03:32 -0500

    updated arrow function for sort

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,9 +1,21 @@
 const findTheOldest = function (people) {
   people.sort((a, b) => {
-    const previous = a.yearOfDeath - a.yearOfBirth;
-    const current = b.yearOfDeath - b.yearOfBirth;
+    if (a.yearOfDeath) {
+      const previous = a.yearOfDeath - a.yearOfBirth;
+    } else {
+      const date = new Date();
+      a.yearOfDeath = date.getFullYear;
+    }
+    if (b.yearOfDeath) {
+      const current = b.yearOfDeath - b.yearOfBirth;
+    } else {
+      const date = new Date();
+      b.yearOfDeath = date.getFullYear;
+    }
     return a > b ? 1 : -1;
+    console.log(a.name, b.name);
   });
+  return people[0].name;
 };
 
 // Do not edit below this line
